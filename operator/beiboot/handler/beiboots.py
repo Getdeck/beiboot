5a75e02d30ee47ae2009a8020c835503ddf6e9db
Michael Schilonka <michael@blueshoe.de>
2022-08-09 22:43:19 +0200

    fix(operator): fix code styling

--- operator/beiboot/handler/beiboots.py
+++ operator/beiboot/handler/beiboots.py
@@ -152,11 +152,13 @@ async def beiboot_created(body, logger, **kwargs):
             else:
                 taken_ports = get_taken_gefyra_ports(custom_api, configuration)
                 try:
-                    gefyra_nodeport = random.choice([
-                        port
-                        for port in range(lower_bound, upper_bound + 1)
-                        if port not in taken_ports
-                    ])
+                    gefyra_nodeport = random.choice(
+                        [
+                            port
+                            for port in range(lower_bound, upper_bound + 1)
+                            if port not in taken_ports
+                        ]
+                    )
                     logger.info(f"Requesting Gefyra Nodeport: {gefyra_nodeport}")
                     services.append(
                         gefyra_service(gefyra_nodeport, namespace, cluster_config)
