1176d936cc9ed7dd35fb065c845732b4290f9d42
Michael Schilonka <michael@blueshoe.de>
2022-08-12 17:10:29 +0200

    chore: bump version to 0.7.3

--- client/beiboot/configuration.py
+++ client/beiboot/configuration.py
@@ -9,7 +9,7 @@ console.setFormatter(formatter)
 logger = logging.getLogger("getdeck.beiboot")
 logger.addHandler(console)
 
-__VERSION__ = "0.7.2"
+__VERSION__ = "0.7.3"
 
 
 class ClientConfiguration(object):
