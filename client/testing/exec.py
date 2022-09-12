2d8d1bf87aaf35fe0063f065df65c97ba1cac5f2
Michael Schilonka <Schille@users.noreply.github.com>
2022-09-12 14:09:06 +0200

    chore: bump docker-py version (#16)

--- client/testing/exec.py
+++ client/testing/exec.py
@@ -1,7 +1,7 @@
 import logging
 import sys
 
-from beiboot.api import create_cluster, remove_cluster
+from beiboot.api import create_cluster, remove_cluster, establish_connection
 
 logger = logging.getLogger("getdeck.beiboot")
 logger.setLevel("DEBUG")
@@ -9,7 +9,11 @@ logger.setLevel("DEBUG")
 
 def create_a_cluster():
     # these are some test ports
-    create_cluster(cluster_name=sys.argv[1], ports=["8080:80"])
+    create_cluster(cluster_name=sys.argv[1], ports=["8090:80"])
+
+
+def connect_a_cluster():
+    establish_connection(cluster_name=sys.argv[1])
 
 
 def remove_a_cluster():
