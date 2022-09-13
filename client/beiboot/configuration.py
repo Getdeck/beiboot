e4d2dc429047c0b0789e830d5a8dc493daf9a0ca
Michael Schilonka <michael@blueshoe.de>
2022-09-13 14:37:35 +0200

    chore: bump version to 0.8.1

--- client/beiboot/configuration.py
+++ client/beiboot/configuration.py
@@ -9,7 +9,7 @@ console.setFormatter(formatter)
 logger = logging.getLogger("getdeck.beiboot")
 logger.addHandler(console)
 
-__VERSION__ = "0.8.0"
+__VERSION__ = "0.8.1"
 
 
 class ClientConfiguration(object):
