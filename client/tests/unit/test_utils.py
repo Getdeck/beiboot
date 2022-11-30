e7915461cb235d3fdab3fe6fe364a7b2318911c3
Michael Schilonka <michael@blueshoe.de>
2022-11-30 16:07:18 +0100

    fix: WIP failing testcases

--- client/tests/unit/test_utils.py
+++ client/tests/unit/test_utils.py
@@ -5,7 +5,7 @@ from beiboot.utils import decode_b64_dict
 def test_beiboot_params():
     data = BeibootParameters.from_raw({"nodes": 3, "maxLifetime": "2h"})
     assert data.nodes == 3
-    assert data.lifetime == "2h"
+    assert data.maxLifetime == "2h"
     assert data.ports is None
 
     _dict = data.as_dict()
