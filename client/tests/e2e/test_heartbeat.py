b7f3bf2b9ff36600a9428f3ae1b4d7f771413500
Michael Schilonka <michael@blueshoe.io>
2023-01-23 19:02:11 +0100

    tests(client): added tests for client
    Signed-off-by: Michael Schilonka <michael@blueshoe.io>

---
+++ client/tests/e2e/test_heartbeat.py
@@ -0,0 +1,64 @@
+from time import sleep
+from datetime import datetime
+import json
+
+from beiboot import api
+from beiboot.types import BeibootRequest, BeibootParameters
+from tests.e2e.base import TestClientBase
+
+from beiboot.types import BeibootState
+from beiboot.configuration import default_configuration
+
+
+class TestBaseSetup(TestClientBase):
+
+    beiboot_name = "mycluster-hb"
+
+    def test_write_heartbeat(self, operator, kubectl, timeout):
+
+        req = BeibootRequest(
+            name=self.beiboot_name,
+            parameters=BeibootParameters(
+                nodes=1,
+                serverStorageRequests="500Mi",
+                serverResources={"requests": {"cpu": "0.25", "memory": "0.25Gi"}},
+                nodeResources={"requests": {"cpu": "0.25", "memory": "0.25Gi"}},
+            ),
+        )
+        bbt = api.create(req)
+
+        bbt.wait_for_state(awaited_state=BeibootState.PENDING, timeout=timeout)
+
+        timestamp = datetime.utcnow()
+
+        api.write_heartbeat("test-client", bbt, timestamp)
+        configmap = kubectl(
+            [
+                "-n",
+                bbt.namespace,
+                "get",
+                "configmap",
+                default_configuration.CLIENT_HEARTBEAT_CONFIGMAP_NAME,
+                "--output",
+                "json",
+            ]
+        )
+        cm = json.loads(configmap)
+        assert "test-client" in cm["data"].keys()
+        assert cm["data"]["test-client"] == str(timestamp.isoformat())
+
+        api.write_heartbeat("test-client1", bbt)
+        configmap = kubectl(
+            [
+                "-n",
+                bbt.namespace,
+                "get",
+                "configmap",
+                default_configuration.CLIENT_HEARTBEAT_CONFIGMAP_NAME,
+                "--output",
+                "json",
+            ]
+        )
+        cm = json.loads(configmap)
+        assert "test-client1" in cm["data"].keys()
+        assert datetime.fromisoformat(cm["data"]["test-client1"])
