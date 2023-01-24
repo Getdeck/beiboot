1e2ee99310d758609df376e6aa82ddfad7dcf7b3
Michael Schilonka <michael@blueshoe.io>
2023-01-24 10:27:38 +0100

    chore: bump version to 1.3.1
    Signed-off-by: Michael Schilonka <michael@blueshoe.io>

--- client/beiboot/configuration.py
+++ client/beiboot/configuration.py
@@ -10,7 +10,7 @@ console.setFormatter(formatter)
 logger = logging.getLogger(__name__)
 logger.addHandler(console)
 
-__VERSION__ = "1.3.0"
+__VERSION__ = "1.3.1"
 
 
 class ClientConfiguration(object):
